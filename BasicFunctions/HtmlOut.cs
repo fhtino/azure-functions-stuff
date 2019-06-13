5da0c42964c137160cabdbb29aac45365829365a
fabry <fabry>
2019-06-13 18:12:09 +0200

    basic functions  (working)

---
+++ BasicFunctions/HtmlOut.cs
@@ -0,0 +1,58 @@
+using System;
+using System.IO;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Hosting;  //<---
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+
+namespace BasicFunctions
+{
+    public static class HtmlOut
+    {
+        [FunctionName("HtmlOut")]
+        public static async Task<IActionResult> Run(
+            [HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)] HttpRequest req,
+            ILogger log,
+            ExecutionContext execContext)
+        {
+            log.LogInformation("C# HTTP trigger function processed a request.");
+
+            string html;
+
+            var mode = req.Query["mode"];
+
+            switch (mode)
+            {
+                case "code":
+                    html = "<html><body><h1>Hello, world</h1></body></html>";
+                    break;
+
+                case "file1":
+                    var hosting = req.HttpContext.RequestServices.GetService(typeof(IHostingEnvironment)) as IHostingEnvironment;
+                    html = File.ReadAllText(Path.Combine(hosting.ContentRootPath, "page1.html"));
+                    break;
+
+                case "file2":
+                    // ExecutionContext - https://github.com/Azure/azure-functions-host/wiki/Retrieving-information-about-the-currently-running-function
+                    html = File.ReadAllText(Path.Combine(execContext.FunctionAppDirectory, "page1.html"));
+                    break;
+
+                default:
+                    html = "default";
+                    break;
+            }
+
+            return
+                new ContentResult()
+                {
+                    Content = html,
+                    ContentType = "text/html",
+                    StatusCode = 200
+                };
+        }
+    }
+}
---
+++ BasicFunctions/_notes.md
@@ -0,0 +1,33 @@
+﻿
+
+### Hosting
+https://github.com/Azure/azure-functions-host/
+
+
+### TODO
+
+Authentication  
+
+https://docs.microsoft.com/en-us/azure/azure-functions/functions-bindings-http-webhook#secure-an-http-endpoint-in-production
+
+
+https://blogs.msdn.microsoft.com/stuartleeks/2018/02/19/azure-functions-and-app-service-authentication/
+https://stackoverflow.com/questions/51413576/azure-functions-authentication
+
+https://docs.microsoft.com/en-us/azure/app-service/overview-authentication-authorization
+
+
+
+
+
+
+EasyAuth not working ???
+
+https://github.com/Azure/azure-functions-host/issues/33
+https://github.com/Azure/azure-functions-host/issues/3898
+
+
+   var xxxx = req.HttpContext.User.Identity.IsAuthenticated;
+            //https://<my azure functions app url>/.auth/me
+
+
