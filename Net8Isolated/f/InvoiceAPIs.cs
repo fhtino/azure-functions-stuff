a7bf7cffaa37728b5191e9fb6f74c55a83669735
Fabrizio Accatino <fabry>
2025-02-20 19:52:23 +0100

    net8isolated

--- Net8Isolated/f/InvoiceAPIs.cs
+++ Net8Isolated/f/InvoiceAPIs.cs
@@ -14,6 +14,7 @@ namespace Net8Isolated
         public DateTime DT { get; set; }
         public string CustomerID { get; set; }
         public double Amount { get; set; }
+        public DateTime UpdateDT { get; set; }
     }
 
 
@@ -45,7 +46,8 @@ namespace Net8Isolated
                         ID = i.ToString("00000"),
                         DT = DateTime.UtcNow,
                         CustomerID = "CUST-" + (i % 4),
-                        Amount = i * 10
+                        Amount = i * 10,
+                        UpdateDT = DateTime.UtcNow
                     });
             }
         }
@@ -75,13 +77,37 @@ namespace Net8Isolated
         public async Task<IActionResult> Insert([HttpTrigger(AuthorizationLevel.Anonymous, "post", Route = _route)] HttpRequest req)
         {
             string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
-            var newInvoice = JsonSerializer.Deserialize<Invoice>(requestBody);
+            var newInvoice = JsonSerializer.Deserialize<Invoice>(requestBody, new JsonSerializerOptions() { PropertyNameCaseInsensitive = true });
+            newInvoice.UpdateDT = DateTime.UtcNow;
             if (newInvoice == null) return new BadRequestResult();
             InvoiceList.Add(newInvoice);
             return new OkObjectResult(newInvoice);
         }
 
+        [Function("InvoiceAPI_Update")]  // *** PUT ***
+        public async Task<IActionResult> Update([HttpTrigger(AuthorizationLevel.Anonymous, "put", Route = _route)] HttpRequest req)
+        {
+            string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
+            var incomingInvoice = JsonSerializer.Deserialize<Invoice>(requestBody, new JsonSerializerOptions() { PropertyNameCaseInsensitive = true });
+            incomingInvoice.UpdateDT = DateTime.UtcNow;
+            var oldInvocie = InvoiceList.SingleOrDefault(x => x.ID == incomingInvoice.ID);
+            if (oldInvocie == null) { return new NotFoundResult(); }
+            InvoiceList.Remove(oldInvocie);
+            InvoiceList.Add(incomingInvoice);
+            return new OkObjectResult(incomingInvoice);
+        }
 
+
+        [Function("InvoiceAPI_Delete")]   // *** DELETE ***
+        public async Task<IActionResult> Delete([HttpTrigger(AuthorizationLevel.Anonymous, "delete", Route = _route + "/{id}")] HttpRequest req, string id)
+        {
+            var item = InvoiceList.FirstOrDefault(x => x.ID == id);
+            if (item == null) return new NotFoundResult();
+            await Task.CompletedTask;
+            InvoiceList.Remove(item);
+            return new OkResult();
+        }
 
     }
+
 }
